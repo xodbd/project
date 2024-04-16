ecacf371c6d2654f39ce4d69a3f4b9aed343ea5a
김태융 <xodbd0507@naver.com>
2024-04-16 22:48:42 +0900

    새로운 프로젝트 주제 반영 및 로그인 기능 구현 중
    새로운 프로젝트 주제 반영 및 로그인 기능 구현 중

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,13 +1,11 @@
-import {useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function Footer() {
-    let navigate = useNavigate();
-
     return (
         <footer>
             <div className="inner">
-                <div className='footer-logo' onClick={()=>navigate("/")}><img src='/img/logo.png' alt=''/></div>
-                <p className="copyright">Copyright 2024. Homeboy._.Doodle inc. all rights reserved.</p>
+                <div className='footer-logo'><Link to={"/"}>R.House</Link></div>
+                <p className="copyright">Copyright 2024. R.House inc. all rights reserved.</p>
             </div>
         </footer>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,83 +1,11 @@
 import { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
-import SwiperCore from "swiper";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, EffectCards } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/effect-cards';
-
-SwiperCore.use([Autoplay, EffectCards]);
 
 function Home() {
-    const nfts = [
-        {
-            id: 1,
-            name : 'Cute Ghost',
-            seller : 'homeboy._.doodle',
-            img : '/img/nft1.jpeg',
-            startPrice : '100000',
-            startDate : '2024/03/30',
-            endDate : '2024/03/31'
-        },
-        {
-            id: 2,
-            name : 'Sprout and Ants',
-            seller : 'homeboy._.doodle',
-            img : '/img/nft2.jpeg',
-            startPrice : '50000',
-            startDate : '2024/03/30',
-            endDate : '2024/03/31'
-        },
-        {
-            id: 3,
-            name : 'Stupid snot boy',
-            seller : 'homeboy._.doodle',
-            img : '/img/nft3.jpeg',
-            startPrice : '80000',
-            startDate : '2024/03/30',
-            endDate : '2024/03/31'
-        }
-    ]
 
     return(
-        <div className="home-wrap">
-            <section className="main">
-                <div className="inner">
-                    <Swiper
-                        spaceBetween={0}
-                        slidesPerView={1}
-                        /* centeredSlides={true}
-                        autoplay={{
-                            delay: 3000,
-                            disableOnInteraction: false,
-                        }} */
-                        effect={'cards'}
-                        grabCursor={true}
-                        loop={true}
-                        modules={[Autoplay, EffectCards]}
-                        className="main-list-wrap"
-                    >
-                        {
-                            nfts.map((prd,i)=>{
-                                return(
-                                    <SwiperSlide className="main-list" key={i}>
-                                        <Link to={`/detail/${prd.id}`}>
-                                            <div className="thumb">
-                                                <img src={prd.img} alt={prd.name} />
-                                            </div>
-                                            <div className="desc">
-                                                <p className="prd-name">{prd.name}</p>
-                                                <p className="prd-start-bid"><span>Starting Bid</span> {prd.startPrice}</p>
-                                                <p className="prd-seller">by. {prd.seller}</p>
-                                            </div>
-                                        </Link>
-                                    </SwiperSlide>
-                                )
-                            })
-                        }
-                    </Swiper>
-                </div>
-            </section>
+        <div className="home-wrap con-wrap">
+            
         </div>
     );
 }
