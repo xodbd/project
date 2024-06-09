981fec3089118884ebe5881b2bbfd40ae2b1aa9d
김태융 <xodbd0507@naver.com>
2024-06-09 13:41:10 +0900

    프로젝트
    프로젝트

---
+++ src/component/cate.js
@@ -0,0 +1,10 @@
+import { Link, useNavigate } from 'react-router-dom';
+export default function Cate(){
+    return (
+        <>
+            <ul className="category">
+                <li className="cate"><Link to={""}></Link></li>
+            </ul>
+        </>
+    )
+}
